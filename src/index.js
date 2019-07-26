2d079af1441906f2a165d0f51393a11c21c80e8d
Vlaicu, Gabriel <Gabriel.Vlaicu@finastra.com>
2019-07-26 16:12:42 +0300

    final

--- src/index.js
+++ src/index.js
@@ -4,7 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebase = require('firebase');
+require('firebase/firestore')
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCT2ByOASWH9R8z23n4SjYf6VYzsdmJiZ4",
+    authDomain: "react-notes-a2563.firebaseapp.com",
+    databaseURL: "https://react-notes-a2563.firebaseio.com",
+    projectId: "react-notes-a2563",
+    storageBucket: "react-notes-a2563.appspot.com",
+    messagingSenderId: "710535014401",
+    appId: "1:710535014401:web:2484cf48cb0963b6"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+ReactDOM.render(<App />, document.getElementById('react-notes'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
